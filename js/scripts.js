03a78f97d24456093285751a7a081f6c090ef36b
Kennedy Martin and Paul Carnevale <kennedy@>
2019-09-09 20:20:12 -0700

    add product receipt files

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
       var name1Input = $("input#name1").val();
+      var address1Input = $("input#address1").val();
 
       $(".name1").text(name1Input);
+      $(".address1").text(address1Input);
 
       $("#letter").show();
 
